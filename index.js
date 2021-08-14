39a73ee3e9aedc7ee570b8fdd382ad13aea9f3cc
Ada <47477123+oohada@users.noreply.github.com>
2021-08-14 17:48:50 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", 
+        function (){
+            var buttonInnerHTML = this.innerHTML;
+            makeSound(buttonInnerHTML);     
+            buttonAnimation(buttonInnerHTML);        
+    }); 
+}
+//detecting keyboard press
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);      
+});
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3"); 
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio("sounds/kick-bass.mp3"); 
+            kick.play();
+            break;                
+        case "s":
+            var kick = new Audio("sounds/snare.mp3"); 
+            kick.play();
+            break;     
+        case "d":
+            var kick = new Audio("sounds/tom-1.mp3"); 
+            kick.play();
+            break;         
+        case "j":
+            var kick = new Audio("sounds/tom-2.mp3"); 
+            kick.play();
+            break;         
+        case "k":
+            var kick = new Audio("sounds/tom-3.mp3"); 
+            kick.play();
+            break;
+        case "l":
+            var kick = new Audio("sounds/tom-4.mp3"); 
+            kick.play();
+            break;                      
+        default:
+            console.log(buttonInnerHTML);
+            break;
+    }
+}
+
+//adding button animation 
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector( "." + currentKey); 
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+   
+
+}
